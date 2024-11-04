f742fdc010d40c5e5108a55d66a51efc07cca2f0
praneed.kla <praneed.kla@northbkk.ac.th>
2024-11-04 17:42:13 +0700

    [fixed]

--- flutter_inappwebview_android/android/src/main/java/com/pichillilorenzo/flutter_inappwebview_android/types/PlatformWebView.java
+++
@@ -1,9 +0,0 @@
-package com.pichillilorenzo.flutter_inappwebview_android.types;
-
-import java.util.HashMap;
-
-import io.flutter.plugin.platform.PlatformView;
-
-public interface PlatformWebView extends PlatformView {
-  void makeInitialLoad(HashMap<String, Object> params);
-}
